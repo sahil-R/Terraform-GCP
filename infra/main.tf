2719df6b8e67388221d462c3d7579fde413a64a0
sahil.ra <sahil.ra@media.net>
2023-11-10 19:57:29 +0530

    changes for nodepool

--- infra/main.tf
+++ infra/main.tf
@@ -54,9 +54,23 @@ terraform {
   }
 }
 
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
 
 resource "google_compute_network" "infra" {
-  name  =   "vpc-network"
+  name  =   "infra"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  depends_on = [
+    google_project_service.compute,
+    google_project_service.container
+  ]
 }
 
 resource "google_compute_subnetwork" "custom-network" {
@@ -71,36 +85,95 @@ resource "google_compute_subnetwork" "custom-network" {
   }
   secondary_ip_range{
     range_name = var.secondary_2
-    ip_cidr_range = var.ip_cidr_range_1
+    ip_cidr_range = var.ip_cidr_range_2
   }
 
 }
 
-resource "google_compute_router" "custom-router" {
-  name = "custom-router"
-  region = var.region
-  network = google_compute_network.infra.id
+resource "google_service_account" "kubernetes" {
+  account_id = "kubernetes"
 }
 
-resource "google_compute_router_nat" "nat" {
-  name = "custom-nat"
-  router = google_compute_router.custom-router.name
-  region = var.region
+# resource "google_compute_router" "custom-router" {
+#   name = "custom-router"
+#   region = var.region
+#   network = google_compute_network.infra.id
+# }
 
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-  nat_ip_allocate_option             = "MANUAL_ONLY"
+# resource "google_compute_router_nat" "nat" {
+#   name = "custom-nat"
+#   router = google_compute_router.custom-router.name
+#   region = var.region
 
-  subnetwork {
-    name                    = google_compute_subnetwork.custom-network.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+#   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+#   nat_ip_allocate_option             = "MANUAL_ONLY"
+
+#   subnetwork {
+#     name                    = google_compute_subnetwork.custom-network.id
+#     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+#   }
+
+#   nat_ips = [google_compute_address.nat.self_link]
+# }
+
+# resource "google_compute_address" "nat" {
+#   name         = "nat"
+#   address_type = "EXTERNAL"
+#   network_tier = "PREMIUM"
+#   depends_on = [google_project_service.compute]
+# }
+
+resource "google_container_cluster" "primary" {
+  name  = "primary"
+  location = var.region
+  initial_node_count = 1
+  remove_default_node_pool = true
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+    name = "primary-nodes"
+    location = var.region
+    cluster = google_container_cluster.primary.id
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 4
+    }
+    node_config {
+    preemptible  = false
+    machine_type = "e2-small"
+
+    labels = {
+      node = "primary"
+    }
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
-
-  nat_ips = [google_compute_address.nat.self_link]
 }
 
-resource "google_compute_address" "nat" {
-  name         = "nat"
-  address_type = "EXTERNAL"
-  network_tier = "PREMIUM"
-  depends_on = [google_project_service.compute]
+resource "google_container_node_pool" "secondary_nodes" {
+    name = "secondary-nodes"
+    location = var.region
+    cluster = google_container_cluster.primary.id
+    autoscaling {
+      min_node_count = 0
+      max_node_count = 2
+    }
+    node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    labels = {
+      node = "secondary"
+    }
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
