8e4ebb9b1aeb320cece73d9f1d821fe36179ec77
sahil.ra <sahil.ra@media.net>
2023-11-10 18:27:27 +0530

    create nat

--- infra/main.tf
+++ infra/main.tf
@@ -76,3 +76,31 @@ resource "google_compute_subnetwork" "custom-network" {
 
 }
 
+resource "google_compute_router" "custom-router" {
+  name = "custom-router"
+  region = var.region
+  network = google_compute_network.infra.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name = "custom-nat"
+  router = google_compute_router.custom-router.name
+  region = var.region
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.custom-network.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  nat_ips = [google_compute_address.nat.self_link]
+}
+
+resource "google_compute_address" "nat" {
+  name         = "nat"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
+  depends_on = [google_project_service.compute]
+}
